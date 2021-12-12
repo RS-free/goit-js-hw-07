3e6df94bb30d160bf88f30b45ab3df309f838665
Ruslan <ruslansulieim@gmail.com>
2021-12-12 23:40:13 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+const galleryBlock = document.querySelector(".gallery");
+const markupGallery = createGalleryMarkup(galleryItems);
+galleryBlock.insertAdjacentHTML("beforeend", markupGallery);
+galleryBlock.addEventListener("click", onGalleryClick);
+function createGalleryMarkup(gallery) {
+    return gallery
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg" onclick="event.preventDefault()">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+        })
+        .join("");
+}
+function onGalleryClick(event) {
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+    `);
+    instance.show();
+    function onKeyEscAction(event) {
+        const ESC_KEY_CODE = "Escape";
+        const isEscKey = event.code === ESC_KEY_CODE;
+        if (isEscKey) {
+            instance.close();
+        }
+    }
+    window.addEventListener("keydown", onKeyEscAction);
+}

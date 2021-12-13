a35e05023b03ce70f36b958d83b3010849d9ff62
Ruslan <ruslansulieim@gmail.com>
2021-12-13 20:40:04 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 console.log(galleryItems);
+const galleryBlock = document.querySelector(".gallery");
+const markupGallery = createGalleryMarkup(galleryItems);
+galleryBlock.insertAdjacentHTML("beforeend", markupGallery);
+galleryBlock.addEventListener("click", onGalleryClick);
+function createGalleryMarkup(gallery) {
+    return gallery
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div>`;
+        })
+        .join("");
+}
+new SimpleLightbox(".gallery__item a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+    scrollZoomFactor: 0.5,
+    captionPosition: "bottom",
+});
+function onGalleryClick(event) {
+    if (!event.target.classList.contains("gallery__item")) {
+        return;
+    }
+}
